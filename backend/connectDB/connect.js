a311ff622f68678b8ad8d2aacc5e57714afaaff4
samironbhowmik <bhowmiksamiron@gmail.com>
2023-01-23 15:04:49 +0530

    cluster link added

--- backend/connectDB/connect.js
+++ backend/connectDB/connect.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose")
 
 mongoose.set("strictQuery", false)
 const connectDB = ()=>{
-    mongoose.connect("mongodb://localhost:27017/instagram").then(()=>{
+    mongoose.connect("mongodb+srv://root:root123@cluster0.jj1siun.mongodb.net/?retryWrites=true&w=majority").then(()=>{
         console.log("Database is connected!");
     })
 }
